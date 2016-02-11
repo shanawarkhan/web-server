4d71250a6f9885457eecba7a75689581230207b7
Shanawarkhan <shanawar_k@hotmail.com>
2016-02-11 06:37:47 +0500

    Update language

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(middleware.logger);
 
 //Using middleware inside about route
 app.get('/about', middleware.requireAuthentication, function(req, res){
-	res.send('About Us');
+	res.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public'));
